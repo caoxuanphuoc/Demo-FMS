07f16d1c536ef33d07e250bed337202541b6ff65
Admin <caophuoc6@gmail.com>
2023-05-19 09:37:29 +0700

    DataStruct: Add Food table

--- aspnet-core/src/FMS.Core/Foods/Food.cs
+++ aspnet-core/src/FMS.Core/Foods/Food.cs
@@ -11,8 +11,10 @@ namespace FMS.Foods
 {
     public class Food : FullAuditedEntity<long>
     {
+        public long CategoryId { get; set; }
+        public Food food { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public long Price { get; set; }
+        public string Price { get; set; }
     }
 }
