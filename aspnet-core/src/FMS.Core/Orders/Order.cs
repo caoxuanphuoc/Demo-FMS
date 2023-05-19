75482bc3f4e1b01fee2dc8304ed4b2bb38211c76
Admin <caophuoc6@gmail.com>
2023-05-19 10:11:53 +0700

    FIx: Add Order Model include ( Order, OrderDetail, StatusOrder)

---
+++ aspnet-core/src/FMS.Core/Orders/Order.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Orders
+{
+    public class Order : FullAuditedEntity<long>
+    {
+        public StatusOrder OrderStatus { get; set; }
+        public DateTime DateTime { get; set; }
+        public long Gross { get; set; }
+    }
+}
