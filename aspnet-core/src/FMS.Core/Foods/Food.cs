e03ad1fc2e3f196d9df7d0497d0393d321dcb291
Admin <caophuoc6@gmail.com>
2023-05-19 10:12:46 +0700

    Destruct: set Attribute for Food table

--- aspnet-core/src/FMS.Core/Foods/Food.cs
+++ aspnet-core/src/FMS.Core/Foods/Food.cs
@@ -11,10 +11,8 @@ namespace FMS.Foods
 {
     public class Food : FullAuditedEntity<long>
     {
-        public long CategoryId { get; set; }
-        public Food food { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public long Price { get; set; }
     }
 }
