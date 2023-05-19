126252e57f2f6de045f6361cb2f499ac3177e9bb
Admin <caophuoc6@gmail.com>
2023-05-19 23:11:53 +0700

    BE: Add TableDetail method

--- aspnet-core/src/FMS.Application/Tables/TableAppService.cs
+++ aspnet-core/src/FMS.Application/Tables/TableAppService.cs
@@ -1,6 +1,9 @@
 ﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
+using FMS.Roles.Dto;
 using FMS.Tables.Dto;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +14,51 @@ namespace FMS.Tables
 {
     public class TableAppService : AsyncCrudAppService<Table, TableDto, long, PagedTableResultRequestDto, CreateTableDto, UpdateTableDto>, ITableAppService
     {
-        public TableAppService(IRepository<Table, long> repository) : base(repository)
+        private readonly IRepository<Table, long> _tableRepository;
+        private readonly IRepository<TableLog, long > _tableLogRepository;
+        public TableAppService(IRepository<Table, long> repository, IRepository<TableLog, long> tableLogRepository) : base(repository)
         {
+            _tableRepository = repository;
+            _tableLogRepository = tableLogRepository;
+        }
+        public  async Task<ListResultDto<TableDetailDto>> GetAll2Async(PagedTableResultRequestDto input)
+        {
+            var LastTimeOfStateTable = await _tableLogRepository.GetAll()
+                                        .GroupBy(Key => new { Key.TableId })
+                                        .Select( x=> new
+                                        {
+                                            TableId = x.Key.TableId,
+                                            Time = x.Max( K => K.CreationTime)
+                                        })
+                                        .ToListAsync();
+         
+            var Table_TableLog = await _tableRepository.GetAll()
+                    .Join(_tableLogRepository.GetAll(),
+                        T => T.Id,
+                        TL => TL.TableId,
+                        (T, TL) => new { Table = T, TableLog = TL })
+                    .Select(x => new TableDetailDto
+                    {
+                        Id = x.Table.Id,
+                        Name = x.Table.Name,
+                        State = x.TableLog.StatusTable.ToString(),
+                        CreationTime = x.TableLog.CreationTime
+                    }
+                ).ToListAsync();
+
+            List<TableDetailDto> res = new List<TableDetailDto>();
+            foreach( var TT in Table_TableLog )
+            {
+                foreach( var T in LastTimeOfStateTable)
+                {
+                    if(TT.Id == T.TableId && TT.CreationTime == T.Time)
+                    {
+                        res.Add(TT);
+                    }
+                }
+
+            }
+            return new ListResultDto<TableDetailDto>(ObjectMapper.Map<List<TableDetailDto>>(res));
         }
     }
 }
