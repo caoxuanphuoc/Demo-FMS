48161f7ea0a9ed9685bbda1dab76f8ed98154cef
Admin <caophuoc6@gmail.com>
2023-05-19 23:22:40 +0700

    FE: [Destruct] add logo, setting router

--- reactjs/src/components/SiderMenu/index.tsx
+++ reactjs/src/components/SiderMenu/index.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { Avatar, Col, Layout, Menu } from 'antd';
 import { L, isGranted } from '../../lib/abpUtility';
 
-import AbpLogo from '../../images/abp-logo-long.png';
+import AbpLogo from '../../images/Logo_quan2.png';
 import { appRouters } from '../../components/Router/router.config';
 import utils from '../../utils/utils';
 
@@ -22,18 +22,18 @@ const SiderMenu = (props: ISiderMenuProps) => {
   const { collapsed, history, onCollapse } = props;
   const currentRoute = utils.getRoute(history.location.pathname);
   return (
-    <Sider trigger={null} className={'sidebar'} width={256} collapsible collapsed={collapsed} onCollapse={onCollapse}>
+    <Sider trigger={null} className={'sidebar'} width={210} collapsible collapsed={collapsed} onCollapse={onCollapse}>
       {collapsed ? (
-        <Col style={{ textAlign: 'center', marginTop: 15, marginBottom: 10 }}>
-          <Avatar shape="square" style={{ height: 27, width: 64 }} src={AbpLogo} />
+        <Col style={{ textAlign: 'center', marginTop: 15, marginBottom: 50 }}>
+          <Avatar shape="square" style={{ height: 10, width: 10, paddingTop: 5 }} src={AbpLogo} />
         </Col>
       ) : (
-        <Col style={{ textAlign: 'center', marginTop: 15, marginBottom: 10 }}>
-          <Avatar shape="square" style={{ height: 54, width: 128 }} src={AbpLogo} />
+        <Col style={{ textAlign: 'center', marginTop: '10px', marginBottom: 10 }}>
+          <Avatar shape="square" style={{ height: 100, width: 128 }} src={AbpLogo} />
         </Col>
       )}
 
-      <Menu theme="dark" mode="inline" selectedKeys={[currentRoute ? currentRoute.path : '']}>
+      <Menu theme="dark" mode="inline" selectedKeys={[currentRoute ? currentRoute.path : '']} >
         {appRouters
           .filter((item: any) => !item.isLayout && item.showInMenu)
           .map((route: any, index: number) => {
