9e1d4688634cf8be384062153fa85fd99fb36b56
Admin <caophuoc6@gmail.com>
2023-05-21 11:07:06 +0700

    BE: Add mapProfile

---
+++ aspnet-core/src/FMS.Application/TableLogs/Dto/TableLogMapProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FMS.Foods.Dto;
+using FMS.Foods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Tables;
+
+namespace FMS.TableLogs.Dto
+{
+    public class TableLogMapProfile : Profile
+    {
+        public TableLogMapProfile()
+        {
+            CreateMap<TableLogDto, TableLog>();
+            CreateMap<CreateTableLogDto, TableLog>();
+        }
+    }
+}
